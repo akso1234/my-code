12d1b78b0c395f3f5a73303b91d8ee04e896d83c
akso1234 <77567577+akso1234@users.noreply.github.com>
2023-05-31 05:19:07 +0000

    05-02

---
+++ 05-02.py
@@ -0,0 +1,8 @@
+korean = ('정렬', '초보자', '내포', '사전')
+english = ('sorting', 'novice', 'comprehension', 'dictionary')
+
+matching = input('찾을 단어 입력 >> ')
+if matching in korean:
+    print(english[korean.index(matching)])
+else:
+    print("단어가 존재하지 않습니다.")
