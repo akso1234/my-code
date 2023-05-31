62313b79151af7617154e29f3d7e556d2b4480df
akso1234 <77567577+akso1234@users.noreply.github.com>
2023-05-31 05:28:21 +0000

    05-08

--- 05-08.py
+++ 05-08.py
@@ -1,2 +1,18 @@
 #20221576 김시온
 import random
+
+ran_list = []
+for i in range(10):
+    ran_list.append(random.randint(1, 101))
+print("리스트:", ran_list)
+
+ran_tuple = tuple(ran_list)
+print("튜플:", ran_tuple)
+print("튜플 정렬된 리스트", sorted(ran_tuple))
+
+print()
+print("합:",sum(ran_tuple), end=', ')
+print("항목수:", len(ran_tuple))
+print("최대:", max(ran_tuple), end=', ')
+print("최소:", min(ran_tuple), end=', ')
+print("평균:", sum(ran_tuple) / 10)
